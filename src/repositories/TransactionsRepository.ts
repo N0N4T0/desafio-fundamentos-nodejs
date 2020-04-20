315361802dd0b36b45eee51ce610691ff268a21e
Lucio Nonato <luciononato@hotmail.com>
2020-04-20 00:14:02 -0300

    1 commit
    desafio concluido

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,23 +6,67 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
+  // "Banco de dados falso"
   private transactions: Transaction[];
 
+  // iniciando uma classe com array vazio para receber dados
   constructor() {
     this.transactions = [];
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
+  // o valor parcial (partial)
+  /**
+  *Parametros do reduce
+  *1- retorna toda a informação salvada no reduce
+   2- como se fosse um map, todos os valores do this.
+   O partial vai ser o que vai se juntar a todos os dados e
+   o transaction é cada transactions dentro do this
+  */
   public getBalance(): Balance {
-    // TODO
+    const { income, outcome } = this.transactions.reduce(
+      (partial: Balance, transaction: Transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            partial.income += transaction.value;
+            break;
+          case 'outcome':
+            partial.outcome += transaction.value;
+            break;
+          default:
+            break;
+        }
+
+        return partial;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    // pega a informação da models(Transaction) e retorna para transaction
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,15 +1,22 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
+import Transaction from '../models/Transaction';
 
 const transactionRouter = Router();
-
-// const transactionsRepository = new TransactionsRepository();
+// instanciando classe
+const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    const transactions = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance();
+
+    return response.json({
+      transactions,
+      balance,
+    });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -17,7 +24,22 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
+    // pegou o corpo
+    const { title, value, type } = request.body;
+
+    // cria o transaction service que cuida da regra de negócio
+    const CreateTransaction = new CreateTransactionService(
+      transactionsRepository,
+    );
+
+    // executar o metodo que retornara a nossa transação criada
+    const transaction = CreateTransaction.execute({
+      title,
+      value,
+      type,
+    });
+
+    return response.json(transaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
